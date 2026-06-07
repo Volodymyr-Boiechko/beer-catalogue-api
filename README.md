da30a00ed3be846f8b81d91620f75e0ae02d1d8d
Volodymyr Boiechko <boiechko17@gmail.com>
2026-06-07 14:43:32 +0200

    chore: bootstrap Spring Boot 3.3 and Java 21 project skeleton

---
+++ README.md
@@ -0,0 +1,25 @@
+# Beer Catalogue API
+
+## Overview
+
+_TODO_
+
+## Setup
+
+_TODO_
+
+## Run with Docker
+
+_TODO_
+
+## API Examples
+
+_TODO_
+
+## Design Decisions
+
+_TODO_
+
+## Trade-offs
+
+_TODO_
---
+++ src/test/java/com/haufe/beercatalogue/BeerCatalogueApplicationTests.java
@@ -0,0 +1,12 @@
+package com.haufe.beercatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerCatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
