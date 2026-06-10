1579914da67050c967ef529df45e33520c7b33d7
Volodymyr Boiechko <boiechko17@gmail.com>
2026-06-10 14:30:39 +0200

    chore(infra): upgrade EKS cluster version 1.30 -> 1.32

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS control plane"
   type        = string
-  default     = "1.30"
+  default     = "1.32"
 }
 
 variable "node_instance_type" {
